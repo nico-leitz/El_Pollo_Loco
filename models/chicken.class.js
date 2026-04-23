9cce96538843341afce448789e24729c46ae9259
nico-leitz <ndesk.ltrz@gmail.com>
2026-04-23 09:57:47 +0200

    feat: added infinite loop to animation for chicken enemies

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,9 +2,26 @@ class Chicken extends MoveableObject {
    positionY = 370;
    height = 60;
    width = 60;
+   IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
+    currentImage = 0;
 
     constructor() {
         super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+        this.loadImages(this.IMAGES_WALKING);
         this.positionX = 200 + Math.random() * 500;
+        this.animate();
+    }
+
+      animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imgCache[path];
+            this.currentImage++;
+        }, 100);
     }
 }
