35fbf5fbce50e9c6d534188f73d804643b9c9f2d
nico-leitz <ndesk.ltrz@gmail.com>
2026-04-23 09:32:24 +0200

    feat: added animation for all images of character

--- models/character.class.js
+++ models/character.class.js
@@ -2,10 +2,31 @@ class Character extends MoveableObject {
     positionY = 140;
     height = 300;
     width = 150;
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
    
     constructor() {
-        super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+        super();
+        this.loadImage("img/2_character_pepe/2_walk/W-21.png");
+        this.loadImages(this.IMAGES_WALKING)
         this.positionX = 100;
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+        let path = this.IMAGES_WALKING[this.currentImage]
+        this.img = this.imgCache[path];
+        this.currentImage++;
+        }, 1000) 
     }
 
     jump() {
--- models/moveable-objects.class.js
+++ models/moveable-objects.class.js
@@ -4,10 +4,19 @@ class MoveableObject {
     height = 150;
     width = 100;
     img;
+    imgCache = {};
 
     loadImage(path) {
         this.img = new Image(); 
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imgCache[path] = img;
+        });
     }
 
     moveLeft() {
