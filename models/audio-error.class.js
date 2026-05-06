17afcfe7edc3a5a63dd8136976c3704fb13220f5
nico-leitz <ndesk.ltrz@gmail.com>
2026-05-06 13:15:56 +0200

    feat: extended AudioError class by playSafe() method

--- models/audio-error.class.js
+++ models/audio-error.class.js
@@ -1,3 +1,14 @@
 class AudioError {
     
+
+
+    static playSafe(sound, volume = 0.05) {
+        let checkReady = setInterval(() => {
+            if (sound.readyState == 4 && !AudioManager.isMuted) {[cite, 5]
+                sound.volume = volume;
+                sound.play();
+                clearInterval(checkReady);
+            }
+        }, 200);
+}
 }
