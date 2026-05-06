f41f892fd14db337691bf0f2db48bb376db88589
nico-leitz <ndesk.ltrz@gmail.com>
2026-05-06 17:19:34 +0200

    feat: implented bottle collections sound in checkCollisionWithBottles method in world.js

--- models/world.class.js
+++ models/world.class.js
@@ -104,6 +104,7 @@ class World {
     checkCollisionsWithBottles() {
         this.level.bottles.forEach((bottle, index) => {
             if (this.character.isColliding(bottle) && this.bottleBar.amount < 100) {
+                AudioManager.play(AudioManager.BOTTLE_COLLECT, 0.1);
                 this.level.bottles.splice(index, 1);
                 this.bottleBar.amount += 20;
                 this.bottleBar.setPercentage(this.bottleBar.amount);
