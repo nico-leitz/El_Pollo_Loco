ae02b1232660c751db0fd2726bace780d9ea3cf6
nico-leitz <ndesk.ltrz@gmail.com>
2026-05-06 12:16:05 +0200

    feat: extend AuidoManger class and implemented play() method

--- models/audio-manager.class.js
+++ models/audio-manager.class.js
@@ -1,4 +1,23 @@
 class AudioManager {
+    static WALKING = new Audio('sounds/character/characterRun.mp3');
+    static JUMPING = new Audio('sounds/character/characterJump.wav');
+    static DAMAGE = new Audio('sounds/character/characterDamage.mp3');
+    static SNORING = new Audio('sounds/character/characterSnoring.mp3');
+    static DEAD = new Audio('sounds/character/characterDead.wav');
+    static CHICKEN_DEAD = new Audio('./assets/sounds/chicken_dead.mp3');
+
+    static allSounds = [
+        AudioManager.WALKING, AudioManager.JUMPING, AudioManager.DAMAGE, 
+        AudioManager.SNORING, AudioManager.DEAD, AudioManager.CHICKEN_DEAD
+    ];
 
     static isMuted = false;
+
+    static play(sound, volume = 0.02) {
+        if (!AudioManager.isMuted) {
+            sound.volume = volume;
+            sound.play();
+        }
+    }
+
 }
